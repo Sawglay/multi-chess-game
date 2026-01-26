589837acb1001b0977adcfcfd61edddcecf708bf
G Lay <sawglay2@gmail.com>
2026-01-26 21:50:18 +0700

    Added Main java & initial package

---
+++ src/com/globalacademy/chessgame/gui/screen/Main.java
@@ -0,0 +1,18 @@
+package com.globalacademy.chessgame.gui.screen;
+
+import javax.swing.SwingUtilities;
+/*Its only job is to start up the Graphical User Interface */
+
+/**
+ * Main class to run the application.
+ * 
+ * @author G Lay, Min Khant, Sonali
+ * @since 2025-08-01
+ * @version UAT
+ */
+
+public class Main {
+    public static void main(String[] args) {/* This line ensures that our user interface is created on the correct thread, */
+        // SwingUtilities.invokeLater(() -> new ModePicker().setVisible(true));
+    }
+}
